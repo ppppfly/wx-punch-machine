185bd2e3f1f9cee135d5479afa11942839052747
Matthew <mpang@gizwits.com>
2017-03-16 10:23:21 +0800

    增加Permission、Role、Membership等概念

--- punch_machine/models.py
+++ punch_machine/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
-class WechatUser(models.Model):
+class TimeMannerModel(models.Model):
+    class Meta:
+        abstract = True
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class WechatUser(TimeMannerModel):
     openid = models.CharField(max_length=50, primary_key=True)
     avatarUrl = models.TextField()
     city = models.CharField(max_length=20, null=True, blank=True)
@@ -12,5 +19,23 @@ class WechatUser(models.Model):
     province = models.CharField(max_length=20, null=True, blank=True)
 
 
-class Group(models.Model):
+class Group(TimeMannerModel):
     name = models.CharField(max_length=50)
+    members = models.ManyToManyField(WechatUser, related_name='groups')
+
+
+class Permission(TimeMannerModel):
+    code = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+
+class Role(TimeMannerModel):
+    name = models.CharField(max_length=10)
+    group = models.ForeignKey(Group)
+    permissions = models.ManyToManyField(Permission, related_name='roles')
+
+
+class Membership(TimeMannerModel):
+    group = models.ForeignKey(Group)
+    WechatUser = models.ForeignKey(WechatUser)
+    role = models.ForeignKey(Role)
--- punch_machine/serializers.py
+++ punch_machine/serializers.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from rest_framework import serializers
 from punch_machine import models
 
@@ -6,8 +7,7 @@ class WechatUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.WechatUser
-        fields = ('openid', 'avatarUrl', 'city', 'country',
-                  'gender', 'language', 'nickName', 'province')
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -15,3 +15,17 @@ class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Group
         fields = '__all__'
+
+
+class GroupCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Group
+        fields = ('name', 'openid')
+
+    openid = serializers.CharField()
+
+    def create(self, validated_data):
+
+        # todo: 绑定当前openid和group,并创建组长角色
+
+        super(GroupCreateSerializer, self).create(validated_data)
